26b3d95ef961d529a9f8c41c33cb8558237adb8f
hingsir <hingsir1024@gmail.com>
2016-06-06 11:23:50 +0800

    Upated at 2016-06-06 11:23:50

---
+++ index.js
@@ -0,0 +1,18 @@
+var exec = require('child_process').exec;
+var moment = require('momentjs')
+
+module.exports = (message) => {
+    exec('git add -A', (err, stdout, stderr) => {
+    	if(err) throw err;
+    	console.log(stdout);
+        message = message || 'Upated at ' + moment().format('f');
+    	exec(`git commit -m "${message}"`, (err, stdout, stderr) => {
+    		if(err) throw err;
+    		console.log(stdout);
+    		exec('git push', (err, stdout, stderr) => {
+    			if(err) throw err;
+    			console.log(stdout);
+    		})
+    	});
+    });
+}
---
+++ readme.md
@@ -0,0 +1,7 @@
+# Push current branch to remote repository on Github
+
+* Usage
+```js
+var push = require('push-to-github')
+push('your commit message')
+```
